c58c1418604732bb4bc9af133238899d0070e3b6
jaejin <jaejin94@ajou.ac.kr>
2022-08-10 00:58:47 +0900

    예제 도메인 모델과 동작확인

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,25 +1,43 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+/*
     // JPA 표준스펙에 엔티티는 기본 생성자가 있어야 한다.
-    protected Member() {
-    }
+    protected Member() {}
+*/
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team; // 객체이기 때문에 이쪽의 team만 바꾸는 것이 아니라
+        team.getMembers().add(this); // Team쪽의 Member도 바꿔줘야함
     }
 }
---
+++ src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,26 @@
+package study.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team") // foreign key가 없는 쪽에 mappedBy 설정권장
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
