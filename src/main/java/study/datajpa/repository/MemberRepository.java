39aeaf21c177e3aa826825158e74694684b312cc
jaejin <jaejin94@ajou.ac.kr>
2022-08-15 22:39:52 +0900

    @EntityGraph

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -16,32 +17,73 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    /**
+     * 메소드 이름으로 쿼리 생성
+     */
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
+    /**
+     * JPA NamedQuery
+     */
     @Query(name = "Member.findByUsername")
     List<Member> findByUsername(@Param("username") String username);
 
+    /**
+     * @Query, 리포지토리 메소드에 쿼리 정의하기
+     */
     @Query("select m from Member m where m.username = :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
 
+    /**
+     * @Query, 값, DTO 조회하기
+     */
     @Query("select m.username from Member m")
     List<String> findUsernameList();
 
     @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t") // 엔티티가 아닌 DTO 조회인 경우 new 키워드를 사용해야함.
     List<MemberDto> findMemberDto();
 
+    /**
+     * 파라미터 바인딩
+     */
     @Query("select m from Member m where m.username in :names")
     List<Member> findByNames(@Param("names") Collection<String> names);
 
+    /**
+     * 반환 타입
+     */
     List<Member> findListByUsername(String username); // 컬렉션
     Member findMemberByUsername(String username); // 단건
     Optional<Member> findOptionalByUsername(String username); // 단건 Optional
 
+    /**
+     * 스프링 데이터 JPA 페이징과 정렬
+     */
     @Query(value = "select m from Member m left join m.team t",
             countQuery = "select count(m) from Member m") // 카운트 쿼리를 날릴 때 join을 안하기 위해 분리함.
     Page<Member> findByAge(int age, Pageable pageable);
 
+    /**
+     * 벌크성 수정 쿼리
+     */
     @Modifying(clearAutomatically = true) // executeUpdate()
     @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
     int bulkAgePlus(@Param("age") int age);
+
+    /**
+     * @EntityGraph
+     */
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    @Override
+    @EntityGraph(attributePaths = {"team"}) // JPQL없이 fetch join 사용하는 방법
+    List<Member> findAll();
+
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findEntityGraphByUsername(@Param("username") String username); // find...ByUsername : ...에는 아무거나 들어가도 됨.
 }
