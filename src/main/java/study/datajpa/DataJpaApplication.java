5b067ba8b05b5a83fe624e56aca3eb893ec43581
jaejin <jaejin94@ajou.ac.kr>
2022-08-16 22:04:26 +0900

    스프링 데이터 JPA Auditing

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,8 +2,14 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
@@ -11,4 +17,16 @@ public class DataJpaApplication {
 		SpringApplication.run(DataJpaApplication.class, args);
 	}
 
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		/* 인터페이스의 메서드가 하나면 람다로 바꿀 수 있음.
+		return new AuditorAware<String>() {
+			@Override
+			public Optional<String> getCurrentAuditor() {
+				return Optional.of(UUID.randomUUID().toString());
+			}
+		};
+		*/
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
 }
